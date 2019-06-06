9ac51c6ee7c289ab10bae321f8a97337fc0219b8
NotifyOne <anton220812@gmail.com>
2019-06-06 17:49:41 +0300

    Reformate code

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ if (isset($_GET['logout'])) {
   header("Location: /");
 }
 
-if (isset($_SESSION['salt'], $_SESSION['login'])
-  && is_int($id = array_search($_SESSION['login'],
-    array_column(users, LOGIN)))
-  && ($_SESSION['salt'] == users[$id][SALT])) {
+if ( isset($_SESSION['salt'], $_SESSION['login'])
+  && is_int( $id = array_search(
+    $_SESSION['login'], array_column(users, LOGIN)
+  ) )
+  && ($_SESSION['salt'] == users[$id][SALT]) ) {
   $username = users[$id][LOGIN];
 
   echo "HELLO, {$username}. You id: {$id}";
@@ -18,9 +19,10 @@ if (isset($_SESSION['salt'], $_SESSION['login'])
   echo '<a href="/?logout">logout</a>';
 }
 elseif (isset($_GET['auth'])) {
-  if (isset($_POST['login'], $_POST['password'])
-    && is_int($id = array_search($_POST['login'],
-      array_column(users, LOGIN)))
+  if ( isset($_POST['login'], $_POST['password'])
+    && is_int($id = array_search(
+      $_POST['login'], array_column(users, LOGIN)
+    ) )
     && ( password_verify($_POST['password'], users[$id][HASH]) ) ) {
 
     $_SESSION[SALT] = users[$id][SALT];
