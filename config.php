ccd3eee179f0fbe142bf18d817550ca51d9d78b4
NotifyOne <anton220812@gmail.com>
2019-06-06 16:44:21 +0300

    Login page in massive

--- config.php
+++ config.php
@@ -1,6 +1,23 @@
 <?php
-define (
-  "root_password",
-  ['hash' => md5("111"),
-    'salt' => md5(md5("123465"))]
-);
+//define (
+//  "root_password",
+//  ['hash' => md5("111"),
+//    'salt' => md5(md5("123465"))]
+//);
+
+define('HASH', "hash");
+define('SALT', "salt");
+define("LOGIN", "login");
+
+define("users",array(
+  [
+    LOGIN => 'root',
+    HASH => (md5('111')),
+    SALT => md5(md5(12311)),
+  ],
+  [
+    LOGIN => 'user1',
+    HASH => (md5('111')),
+    SALT => md5(md5(1231231)),
+  ],
+));
--- index.php
+++ index.php
@@ -9,15 +9,24 @@ if (isset($_GET['logout'])) {
 }
 
 
-if (isset($_SESSION['salt']) && ($_SESSION['salt'] == root_password['salt'])) {
+if (isset($_SESSION['salt'])
+  && ( isset($_SESSION['login']) && is_int($id = array_search($_SESSION['login'],
+    array_column(users, LOGIN))) )
+  && (isset($_SESSION['salt']) && ($_SESSION['salt'] == users[$id][SALT]) ) ) {
+  $username = users[$id][LOGIN];
+
+  echo "HELLO, {$username}. You id: {$id}";
+  echo '<br>';
   echo '<a href="/?logout">logout</a>';
 }
 elseif (isset($_GET['auth'])) {
   if (
-    isset($_POST['login']) && ($_POST['login'] == 'root')
+    isset($_POST['login']) && is_int( $id = array_search($_POST['login'],
+      array_column(users, LOGIN)) )
     && isset($_POST['password'])
-    && (md5($_POST['password']) == root_password['hash']) ) {
-      $_SESSION['salt'] = root_password['salt'];
+    && (md5($_POST['password']) == users[$id][HASH]) ) {
+      $_SESSION[SALT] = users[$id][SALT];
+      $_SESSION[LOGIN] = users[$id][LOGIN];
     }
   header("Location: /");
 }
